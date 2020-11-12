c0881bcfe73c5d73853315c962f27bfc613972f2
Radu M <root@radu.sh>
2020-11-12 00:16:30 -0800

    Add examples for linking import objects
    Signed-off-by: Radu M <root@radu.sh>

---
+++ examples/wasi-example.js
@@ -0,0 +1,18 @@
+"use strict";
+const { WASI } = require("wasi");
+const { Linker } = require("../dist/src/index");
+const assert = require("assert");
+const fs = require("fs");
+const wasi = new WASI();
+
+(async () => {
+  var linker = new Linker();
+  linker.imports({ wasi_snapshot_preview1: wasi.wasiImport });
+
+  const wasi_hello = await WebAssembly.compile(
+    fs.readFileSync("./wasi_hello.wasm")
+  );
+
+  var instance = await linker.instantiate(wasi_hello);
+  wasi.start(instance);
+})();
