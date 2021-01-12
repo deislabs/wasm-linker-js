8b3312052cac415fe4aedd5fc50fa17022352aa1
Radu M <root@radu.sh>
2021-01-12 13:44:58 -0800

    Update asycify-wasm version
    This commit bumps asycify-wasm to 1.1.2, which now includes type
    definitions, meaning we can remove them from this project.
    
    Signed-off-by: Radu M <root@radu.sh>

--- src/@types/asyncify-wasm/index.d.ts
+++
@@ -1,4 +0,0 @@
-export class Instance extends WebAssembly.Instance {}
-
-export const instantiate: typeof WebAssembly.instantiate;
-export const instantiateStreaming: typeof WebAssembly.instantiateStreaming;
